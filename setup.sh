98f82a43175312bc69471cdab880ec927a366270
Antonio Mikley <antonio.mikley@gmailcom>
2024-02-09 11:48:34 +0100

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
-read -s -p "Enter user password:" userPass
+read -s -p "Enter ansible_become_password:" userPass
 echo ""
 echo "--- Installing stow and ansible ---"
-dnf install -y stow ansible
+sudo dnf install -y stow ansible
 echo "--- Setting up with ansible ---"
 ansible-playbook setup_system.yml -e "ansible_become_password=$userPass"
 
