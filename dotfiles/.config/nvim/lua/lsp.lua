6873acfd4054db79deaadc5ac06a9ced1694f21c
Antonio Mikley <antonio.mikley@gmail.com>
2024-03-04 10:42:38 +0100

    play around with cmp and rust_analyzer

--- dotfiles/.config/nvim/lua/lsp.lua
+++ dotfiles/.config/nvim/lua/lsp.lua
@@ -1,8 +1,23 @@
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 local format = function(client, bufnr)
     require("lsp-format").on_attach(client, bufnr)
 end
 local lsp = require("lspconfig")
-lsp.rust_analyzer.setup({ capabiliies = capabilities, on_attach = format })
-lsp.lua_ls.setup({ capabiliies = capabilities, on_attach = format })
+lsp.rust_analyzer.setup({
+    cmd = { "rust-analyzer" },
+    capabilities = capabilities,
+    on_attach = format,
+    settings = {
+        ["rust_analyzer"] = {
+            diagnostics = {
+                enable = false,
+            },
+            checkOnSave = {
+                command = "clippy",
+            }
+        }
+    }
+})
+lsp.lua_ls.setup({ capabilities = capabilities, on_attach = format })
 lsp.texlab.setup({ capabilities = capabilities, on_attach = format })
--- dotfiles/.config/nvim/lua/plugins/nvim-cmp.lua
+++ dotfiles/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -5,7 +5,9 @@ return {
             "L3MON4D3/LuaSnip",
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua"
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
+            'saadparwaiz1/cmp_luasnip'
         },
         config = function()
             local cmp = require("cmp")
@@ -15,6 +17,7 @@ return {
                         require("luasnip").lsp_expand(args.body)
                     end,
                 },
+                window = {},
                 mapping = cmp.mapping.preset.insert({
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<Tab>"] = cmp.mapping.select_next_item(),
@@ -24,10 +27,9 @@ return {
                 sources = cmp.config.sources({
                     {
                         name = "nvim_lsp",
-                        entry_filter = function(entry)
-                            return cmp.lsp.CompletionItemKind.Snippet ~= entry:get_kind()
-                        end
                     },
+                    { name = "nvim_lsp_signature_help" },
+                    { name = "luasnip" },
                     { name = "path" },
                     { name = "nvim_lua" }
                 })
